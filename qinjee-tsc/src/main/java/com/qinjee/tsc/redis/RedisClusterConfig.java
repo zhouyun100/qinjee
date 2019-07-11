fb2bfb330f67b4491b45495150d18a9023bb4897
admin <zhouyun@qinjee.cn>
2019-07-11 12:23:57 +0800

    修改RedisClusterConfig连接池配置信息

--- qinjee-tsc/src/main/java/com/qinjee/tsc/redis/RedisClusterConfig.java
+++ qinjee-tsc/src/main/java/com/qinjee/tsc/redis/RedisClusterConfig.java
@@ -10,6 +10,7 @@
  */
 package com.qinjee.tsc.redis;
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,9 +27,10 @@ public class RedisClusterConfig {
     private String clusterNodes;
 
     @Bean
-    public JedisCluster getJedisCluster() {
+    public JedisCluster jedisCluster() {
         //分割集群节点
         String[] cNodes = clusterNodes.split(",");
+
         //创建set集合对象
         Set<HostAndPort> nodes = new HashSet<>();
         for (String node : cNodes) {
@@ -36,8 +38,24 @@ public class RedisClusterConfig {
             nodes.add(new HostAndPort(hp[0], Integer.parseInt(hp[1])));
         }
 
-        //创建Redis集群对象
-        JedisCluster jedisCluster = new JedisCluster(nodes);
+        //创建连接池配置对象
+        GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+        //连接耗尽时是否阻塞, false报异常,ture阻塞直到超时, 默认true
+        poolConfig.setBlockWhenExhausted(false);
+        //最大连接数
+        poolConfig.setMaxTotal(1000);
+        //最大空闲数
+        poolConfig.setMaxIdle(200);
+        //最大空闲数
+        poolConfig.setMinIdle(50);
+        //最大建立连接等待时间
+        poolConfig.setMaxWaitMillis(3000);
+        //是否在从池中取出连接前进行检验,如果检验失败,则从池中去除连接并尝试取出另一个
+        poolConfig.setTestOnBorrow(false);
+
+        //创建Redis集群对象，timeout默认2000
+        JedisCluster jedisCluster = new JedisCluster(nodes, poolConfig);
+
         return jedisCluster;
     }
 
