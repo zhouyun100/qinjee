71ba418d6e6c7665f8344f2273ac047a61d033e7
admin <zhouyun@qinjee.cn>
2019-07-16 14:05:32 +0800

    1、yml添加druid连接池配置信息 2、Queue绑定添加@Configuration 3、redis集群取消一次del多个key的service方法

--- qinjee-tsc/src/main/java/com/qinjee/tsc/rabbitmq/RabbitQueueBinding.java
+++ qinjee-tsc/src/main/java/com/qinjee/tsc/rabbitmq/RabbitQueueBinding.java
@@ -16,7 +16,9 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RabbitQueueBinding extends RabbitExchange{
 
     /**
