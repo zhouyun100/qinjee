ce8aa3bd02857db597a05be3678ba12045337011
gx1360414553 <1360414553@qq.com>
2019-08-20 15:32:30 +0800

    新增返回json空值去掉null和""配置类

---
+++ qinjee-common/src/main/java/com/qinjee/config/JacksonConfig.java
@@ -0,0 +1,27 @@
+package com.qinjee.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+/**
+ * @author 高雄
+ * @version 1.0.0
+ * @Description 返回json空值去掉null和""
+ * @createTime 2019年08月20日 15:29:00
+ */
+@Configuration
+public class JacksonConfig {
+    @Bean
+    @Primary
+    @ConditionalOnMissingBean(ObjectMapper.class)
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return objectMapper;
+    }
+}
