7184993d9c5acba6802ba937f2f2a1ccfbf475b6
gx1360414553 <1360414553@qq.com>
2019-09-09 11:06:56 +0800

    数据中心新增swagger配置 删除无用导包信息

--- qinjee-masterdata/src/main/java/com/qinjee/masterdata/swagger/Swagger2Config.java
+++ qinjee-masterdata/src/main/java/com/qinjee/masterdata/swagger/Swagger2Config.java
@@ -14,8 +14,6 @@
  */
 package com.qinjee.masterdata.swagger;
 
-import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
