5ba7dde33ad79b5c073d24388b4ab43ee1395f78
dreamanddistance <17629926998@163.com>
2019-09-10 15:32:11 +0800

    controller新建

--- qinjee-masterdata/src/main/java/com/qinjee/masterdata/controller/StaffController/StaffPreEmploymentController.java
+++ qinjee-masterdata/src/main/java/com/qinjee/masterdata/controller/StaffController/StaffPreEmploymentController.java
@@ -1,6 +1,7 @@
 package com.qinjee.masterdata.controller.StaffController;
 
 import com.qinjee.masterdata.controller.BaseController;
+import com.qinjee.model.response.ResponseResult;
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,5 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/staff")
 @Api("人员相关接口")
 public class StaffPreEmploymentController extends BaseController {
-
+   public ResponseResult insertPreEmploymentList(){
+       return null;
+   }
 }
