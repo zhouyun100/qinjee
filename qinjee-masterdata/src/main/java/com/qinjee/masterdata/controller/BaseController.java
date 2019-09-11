6a9c34b65b55d8812f0795a5d568c979cac2bea3
gx1360414553 <1360414553@qq.com>
2019-09-11 15:10:57 +0800

    职位体系swagger接口文档接口初步编写完成

--- qinjee-masterdata/src/main/java/com/qinjee/masterdata/controller/BaseController.java
+++ qinjee-masterdata/src/main/java/com/qinjee/masterdata/controller/BaseController.java
@@ -10,15 +10,22 @@
  */
 package com.qinjee.masterdata.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.qinjee.consts.ResponseConsts;
+import com.qinjee.masterdata.redis.RedisClusterService;
 import com.qinjee.model.request.UserSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
-//@Component
+@Component
 public class BaseController {
 
-//    @Autowired
-//    private RedisClusterService redisClusterService;
+    @Autowired
+    private RedisClusterService redisClusterService;
 
     public UserSession userSession;
 
@@ -29,24 +36,24 @@ public class BaseController {
      * @return
      */
     public UserSession getUserSession(HttpServletRequest request){
-//        Cookie[] cookies = request.getCookies();
-//        if (cookies != null) {
-//            for (int i = 0; i < cookies.length; i++) {
-//                if (ResponseConsts.SESSION_KEY.equals(cookies[i].getName())) {
-//                    userSession = new UserSession();
-//
-//                    JSONObject jsonObject = JSON.parseObject(redisClusterService.get(cookies[i].getValue()));
-//                    userSession.setUserId(Integer.valueOf(String.valueOf(jsonObject.get("userId"))));
-//                    userSession.setUserName(String.valueOf(jsonObject.get("userName")));
-//                    userSession.setPhone(String.valueOf(jsonObject.get("phone")));
-//                    userSession.setEmail(String.valueOf(jsonObject.get("email")));
-//                    userSession.setCompanyId(Integer.valueOf(String.valueOf(jsonObject.get("companyId"))));
-//                    userSession.setArchiveId(Integer.valueOf(String.valueOf(jsonObject.get("archiveId"))));
-//
-//                    return userSession;
-//                }
-//            }
-//        }
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (int i = 0; i < cookies.length; i++) {
+                if (ResponseConsts.SESSION_KEY.equals(cookies[i].getName())) {
+                    userSession = new UserSession();
+
+                    JSONObject jsonObject = JSON.parseObject(redisClusterService.get(cookies[i].getValue()));
+                    userSession.setUserId(Integer.valueOf(String.valueOf(jsonObject.get("userId"))));
+                    userSession.setUserName(String.valueOf(jsonObject.get("userName")));
+                    userSession.setPhone(String.valueOf(jsonObject.get("phone")));
+                    userSession.setEmail(String.valueOf(jsonObject.get("email")));
+                    userSession.setCompanyId(Integer.valueOf(String.valueOf(jsonObject.get("companyId"))));
+                    userSession.setArchiveId(Integer.valueOf(String.valueOf(jsonObject.get("archiveId"))));
+
+                    return userSession;
+                }
+            }
+        }
         return null;
     }
 }
