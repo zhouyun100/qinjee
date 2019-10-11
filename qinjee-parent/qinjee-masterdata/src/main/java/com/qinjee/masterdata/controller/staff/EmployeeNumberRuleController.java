833a75e221c8f36238846b1beb9737daeca3dc62
admin <zhouyun@qinjee.cn>
2019-10-11 15:50:26 +0800

    项目结构优化，删除外层公共jar包

--- qinjee-parent/qinjee-masterdata/src/main/java/com/qinjee/masterdata/controller/staff/EmployeeNumberRuleController.java
+++ qinjee-parent/qinjee-masterdata/src/main/java/com/qinjee/masterdata/controller/staff/EmployeeNumberRuleController.java
@@ -7,6 +7,7 @@ import com.qinjee.model.response.ResponseResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +27,7 @@ public class EmployeeNumberRuleController extends BaseController {
     @Autowired
     private EmployeeNumberRuleService employeeNumberRuleService;
 
-    @RequestMapping("/addEmployeeNumberRule")
+    @PostMapping("/addEmployeeNumberRule")
     @ApiOperation(value = "新增员工规则", notes = "高雄")
     public ResponseResult addEmployeeNumberRule(EmployeeNumberRuleVo employeeNumberRuleVo){
         return employeeNumberRuleService.addEmployeeNumberRule(employeeNumberRuleVo, getUserSession());
