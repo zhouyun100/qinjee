59fca294169a3cd8eded5f5a9ebbf16ce2f21411
admin <zhouyun@qinjee.cn>
2019-10-23 10:29:43 +0800

    邮件账号密码密文解密

--- qinjee-parent/qinjee-masterdata/src/main/java/com/qinjee/masterdata/service/email/impl/EmailConfigServiceImpl.java
+++ qinjee-parent/qinjee-masterdata/src/main/java/com/qinjee/masterdata/service/email/impl/EmailConfigServiceImpl.java
@@ -13,6 +13,7 @@ package com.qinjee.masterdata.service.email.impl;
 import com.qinjee.masterdata.dao.email.EmailConfigDao;
 import com.qinjee.masterdata.model.entity.EmailConfig;
 import com.qinjee.masterdata.service.email.EmailConfigService;
+import com.qinjee.utils.AesUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -35,11 +36,19 @@ public class EmailConfigServiceImpl implements EmailConfigService {
     public EmailConfig getEmailConfigByCompanyId(Integer companyId) {
         EmailConfig emailConfig = null;
         if(companyId != null){
-            emailConfig = emailConfigDao.getEmailConfigByCompanyId(companyId);
+            try {
+                emailConfig = emailConfigDao.getEmailConfigByCompanyId(companyId);
 
-            if(emailConfig == null){
-                // 获取平台默认的邮件配置信息(当企业未配置邮件信息时使用平台默认配置)
-                emailConfig = emailConfigDao.getEmailConfigByCompanyId(COMPANY_QINJEE_1);
+                if(emailConfig == null){
+                    // 获取平台默认的邮件配置信息(当企业未配置邮件信息时使用平台默认配置)
+                    emailConfig = emailConfigDao.getEmailConfigByCompanyId(COMPANY_QINJEE_1);
+                }
+
+                // 邮件账号密码密文解密
+                emailConfig.setPassword(AesUtils.aesDecode(emailConfig.getPassword(),emailConfig.getAccount()));
+
+            }catch (Exception e){
+                e.printStackTrace();
             }
         }
         return emailConfig;
