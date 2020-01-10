2259f1d94923ef77586118942ab8582b2fa069d5
penghs520 <1546795587@qq.com>
2020-01-10 18:26:48 +0800

    用户信息bug修复

--- qinjee-parent/qinjee-masterdata/src/main/java/com/qinjee/masterdata/service/organation/impl/UserArchiveServiceImpl.java
+++ qinjee-parent/qinjee-masterdata/src/main/java/com/qinjee/masterdata/service/organation/impl/UserArchiveServiceImpl.java
@@ -157,7 +157,7 @@ public class UserArchiveServiceImpl extends AbstractOrganizationHelper<UserArchi
         BeanUtils.copyProperties(userArchiveVo, userArchiveVo1);
         BeanUtils.copyProperties(userArchiveVo1, userArchive);
         logger.info("编辑的userArchive："+userArchive);
-        userArchiveDao.updateByPrimaryKeySelective(userArchive);
+        userArchiveDao.updateByPrimaryKey(userArchive);
     }
 
     @Override
