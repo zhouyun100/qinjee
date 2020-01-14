b21d1cdf400901191736c1ef53def520094c7c33
admin <zhouyun@qinjee.cn>
2020-01-14 17:11:56 +0800

    新增档案时，验证证件号码是否已存在--代码注销，逻辑有问题，换新接口

--- qinjee-parent/qinjee-masterdata/src/main/java/com/qinjee/masterdata/service/staff/impl/StaffCommonServiceImpl.java
+++ qinjee-parent/qinjee-masterdata/src/main/java/com/qinjee/masterdata/service/staff/impl/StaffCommonServiceImpl.java
@@ -323,12 +323,12 @@ public class StaffCommonServiceImpl implements IStaffCommonService {
                                     }
                                 }
 
-                                if(org.apache.commons.lang3.StringUtils.isNoneBlank(userArchive.getIdType()) && org.apache.commons.lang3.StringUtils.isNoneBlank(userArchive.getIdNumber())){
-                                    Integer id = userArchiveDao.selectByIDNumberAndCompanyId(userArchive.getIdType(), userArchive.getIdNumber(), userSession.getCompanyId ());
-                                    if(id != null && !id.equals(archiveId)){
-                                        ExceptionCast.cast ( CommonCode.IDNUMBER_ALREADY_EXIST );
-                                    }
-                                }
+//                                if(org.apache.commons.lang3.StringUtils.isNoneBlank(userArchive.getIdType()) && org.apache.commons.lang3.StringUtils.isNoneBlank(userArchive.getIdNumber())){
+//                                    Integer id = userArchiveDao.selectByIDNumberAndCompanyId(userArchive.getIdType(), userArchive.getIdNumber(), userSession.getCompanyId ());
+//                                    if(id != null && !id.equals(archiveId)){
+//                                        ExceptionCast.cast ( CommonCode.IDNUMBER_ALREADY_EXIST );
+//                                    }
+//                                }
                                 userArchiveDao.insertSelective ( userArchive );
                                 checkEmployeeNumber ( userSession, userArchive );
                                 userArchiveDao.updateByPrimaryKeySelective ( userArchive );
